9fb0a540f181726731a3b88b28e06d6197e38917
Rakan Alotibi <rakan.w10@outlook.com>
2024-11-17 09:51:20 +0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:poc_go_router/screen/login_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
